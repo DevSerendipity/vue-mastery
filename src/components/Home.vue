22456f75db5386d7f0ee848a1841d622dee1dbeb
IslamSakrak <islamsakrak2002@gmail.com>
2021-09-20 10:06:33 +0200

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Home</div>
+    <p>Name in the store is : {{ name }} </p>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+
+const store = useStore()
+
+const name = computed(() => {
+    return store.state.user.name
+})
+</script>
---
+++ src/spring/notesharing/src/test/java/com/notesharing/notesharing/NotesharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notesharing.notesharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
