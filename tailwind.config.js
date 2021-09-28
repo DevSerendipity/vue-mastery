b0df55183dcb652f2070e6cf9d288135c658248d
IslamSakrak <islamsakrak2002@gmail.com>
2021-09-28 10:17:02 +0200

    setting up the mainframe color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   purge: ['./index.html','./src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'primary': '#C4C4C4'
+    }),
+    
     extend: {},
   },
   variants: {
