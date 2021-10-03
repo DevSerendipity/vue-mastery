d4f16f1e57d07bf8ce33d3824d10a2b65834c025
IslamSakrak <islamsakrak2002@gmail.com>
2021-10-03 17:47:52 +0200

    created the user profile and made is also reponsive to all screen sizes

---
+++ src/components/Profile.vue
@@ -0,0 +1,30 @@
+<template>
+    <div
+        class="w-full h-24 bg-primary text-black font-bold flex justify-center items-center"
+    >USER PROFILE</div>
+    <div class="w-full h-full flex justify-center items-center mt-10 sm:mt-5 md:mt-8 lg:mt-10 xl:mt-16 2xl:mt-20">
+        <img
+            class="w-max h-20 sm:h-24 md:h-28 lg:h-36 xl:h-44 2xl:h-72 mb-5"
+            src="src/assets/UserProfileImage.jpg"
+            alt="Profile Image"
+        />
+    </div>
+
+    <div class="flex flex-row w-full h-full">
+        <div class="w-full h-full flex flex-col justify-center items-center 2xl:mx-20">
+            <div class="w-4/6 h-10 bg-primary m-4 rounded flex justify-center items-center my-10">BRO</div>
+            <div class="w-4/6 h-10 bg-primary m-4 rounded flex justify-center items-center my-10">BRO</div>
+            <div class="w-4/6 h-10 bg-primary m-4 rounded flex justify-center items-center my-10">BRO</div>
+        </div>
+        <div class="w-full h-full flex flex-col justify-center items-center 2xl:mx-20">
+            <div class="w-4/6 h-10 bg-primary m-4 rounded flex justify-center items-center my-10">BRO</div>
+            <div class="w-4/6 h-10 bg-primary m-4 rounded flex justify-center items-center my-10">BRO</div>
+            <div class="w-4/6 h-10 bg-primary m-4 rounded flex justify-center items-center my-10">BRO</div>
+        </div>
+    </div>
+    <div class="w-full h-max flex justify-around my-10 mb-20 ">
+        <div
+            class="w-full mx-11 sm:mx-14 md:mx-16 lg:mx-20 xl:mx-28 2xl:mx-48 h-10 bg-primary rounded flex justify-center items-center"
+        >BRO</div>
+    </div>
+</template>
