3445cb76c547f7bc0da54a990a5a5214369cfc58
IslamSakrak <islamsakrak2002@gmail.com>
2021-10-03 19:23:48 +0200

    Refactored the name

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,11 +1,16 @@
 <template>
-<div class="w-full h-full flex justify-center items-center"> 
-    <img class="w-max h-32 flex justify-center items-center mt-20" src="https://c.tenor.com/jaVORWcTiyEAAAAM/cute-fingers.gif">
-</div>
-<p class="h-16 flex justify-center items-center" >UMM I THINK...YOU HAVE MISSTYPED , TRY ONE OF THESE</p>
-<div class="h-5 flex justify-center items-center ">
-    <router-link class="mx-10" to="/">Go to Home</router-link>
-    <router-link class="mx-10" to="/note">Go to Note</router-link>
-    <router-link class="mx-10" to="/profile">Go to Profile</router-link>
+    <div class="w-full h-full flex justify-center items-center">
+        <img
+            class="w-max h-32 flex justify-center items-center mt-20"
+            src="https://c.tenor.com/jaVORWcTiyEAAAAM/cute-fingers.gif"
+        />
+    </div>
+    <p
+        class="h-16 flex justify-center items-center"
+    >UMM I THINK...YOU HAVE MISSTYPED , TRY ONE OF THESE</p>
+    <div class="h-5 flex justify-center items-center">
+        <router-link class="mx-10" to="/">Go to Home</router-link>
+        <router-link class="mx-10" to="/note">Go to Note</router-link>
+        <router-link class="mx-10" to="/profile">Go to Profile</router-link>
     </div>
 </template>
