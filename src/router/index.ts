b4f4e88cd19de7d0806d613dcd65f89e2527d241
IslamSakrak <islamsakrak2002@gmail.com>
2021-10-03 17:48:33 +0200

    added the routes for the web app that you need to manuallz enter

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from '/src/components/Home.vue'
+import Note from '/src/components/Note.vue'
+import Profile from '/src/components/Profile.vue'
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home,
+    },
+    {
+        path: '/note',
+        name: 'Note',
+        component: Note,
+    },
+    {
+        path: '/profile',
+        name: 'Profile',
+        component: Profile,
     }
+
 ]
 
 const router = createRouter({
